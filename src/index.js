ea18e4fd561831897a6dfea6a38cb183fa6349d9
Andriy_Step <andystep2008@gmail.com>
2022-12-31 20:40:07 +0200

    fixing emailjs3

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import emailjs from '@emailjs/browser';
-
+import emailjs from "emailjs-com";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
