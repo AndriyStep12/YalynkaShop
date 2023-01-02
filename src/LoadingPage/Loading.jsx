25c517e4d8195c816c8a5e589e76db434dfc1b49
Andriy_Step <andystep2008@gmail.com>
2023-01-02 23:09:52 +0200

    fixing

--- src/LoadingPage/Loading.jsx
+++ src/LoadingPage/Loading.jsx
@@ -16,7 +16,7 @@ function LoadingScreen (){
         $(document.body).css("overflow-y", "scroll")
         let loadPage = document.getElementById("loadScreen")
         $(loadPage).css("top", "-100vh")
-    }, 16000)
+    }, 9000)
 
     return(
         <div id='loadScreen' className="loadScreen">
