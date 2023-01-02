c6dbcd675c08e23a31c21a4e88bc63041ba015d5
Andriy_Step <andystep2008@gmail.com>
2023-01-02 10:02:20 +0200

    button css

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -14,27 +14,7 @@ import PropsBoxInfo from './PropsBoxHome/PropsBoxHome'
 
 function Home (){
 
-    const rippleButton = document.querySelector(".ripple-button");
-
-    function mousePositionToCustomProp(event, element) {
-        let posX = event.offsetX;
-        let posY = event.offsetY;
-
-        element.style.setProperty("--x", posX + "px");
-        element.style.setProperty("--y", posY + "px");
-    }
-
-    rippleButton.addEventListener("click", (e) => {
-        mousePositionToCustomProp(e, rippleButton);
-        rippleButton.classList.add("pulse");
-        rippleButton.addEventListener(
-        "animationend",
-        () => {
-            rippleButton.classList.remove("pulse");
-        },
-        { once: true }
-        );
-    });
+    
 
 
     const el = useRef(null)
