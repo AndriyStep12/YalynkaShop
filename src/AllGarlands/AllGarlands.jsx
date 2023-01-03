0138b51a193f3722782573e36245e0e8b847ff46
Andriy_Step <andystep2008@gmail.com>
2023-01-03 13:48:24 +0200

    update fixing

--- src/AllGarlands/AllGarlands.jsx
+++ src/AllGarlands/AllGarlands.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import $ from 'jquery'
-// import './AllTree.css'
-import saleImg from './sale.png'
 import jQuery from "jquery";
-import tovarsArr from '../Base/Tovars/Garlands'
+import garlandArr from '../Base/Tovars/Garlands'
 import Box from '../Box/Box'
 
 let arr = JSON.parse(localStorage.getItem('arr')) || [];
@@ -13,7 +11,7 @@ class AllGarlands extends Component{
     render(){
         return(
             <div className="alltreeBase">
-                {tovarsArr.map(item => <Box box={item.box} key={item} urlPhoto={item.urlPhoto} name={item.name} price={item.price} locationTovar={item.locationTovar} nameClass={item.nameClass} inputNumb={item.inputNumb} />)}
+                {garlandArr.map(item => <Box box={item.box} key={item} urlPhoto={item.urlPhoto} name={item.name} price={item.price} locationTovar={item.locationTovar} nameClass={item.nameClass} inputNumb={item.inputNumb} />)}
             </div>
         )
     }
