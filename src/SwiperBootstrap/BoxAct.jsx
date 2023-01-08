ba19aaedead2a936e0b1f454e1f929d9580faf27
Andriy_Step <andystep2008@gmail.com>
2023-01-08 23:33:12 +0200

    bgi

--- src/SwiperBootstrap/BoxAct.jsx
+++ src/SwiperBootstrap/BoxAct.jsx
@@ -7,7 +7,7 @@ import $ from 'jquery'
 function BoxAct (props) {
     return(
         <div className={props.clasName}>
-            <div className="boxAct">
+            <div className="boxAct" style="background-image: url(https://media.istockphoto.com/id/1336074248/photo/blue-christmas-and-new-year-holiday-frame.jpg?b=1&s=170667a&w=0&k=20&c=BQHrKhSk2CU54Pduw4ss9ObCqfoW2jLPEwi2T-5hAp4=)">
                 <h3>{props.nameAction}</h3>
                 <p>{props.description}</p>
             </div>
