37dcc93883ff1111f71507fedf839c3ff0ba94f9
Andriy_Step <andystep2008@gmail.com>
2023-01-08 21:23:31 +0200

    props fixing

--- src/SwiperBootstrap/BoxAct.jsx
+++ src/SwiperBootstrap/BoxAct.jsx
@@ -1,7 +1,15 @@
-export default (props) => {
+import React from "react"
+import { ReactDOM } from "react"
+import { PropsWithChildren } from "react"
+import jQuery from "jquery";
+import $ from 'jquery'
+
+function BoxAct (props) {
     return(
         <div className="boxAct">
             <h3>{this.props.nameAction}</h3>
         </div>
     )
 }
+
+export default BoxAct;
