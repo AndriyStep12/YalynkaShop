a94b96542c44a2b63a0d879be9c6cf3e9b71aaf4
Andriy_Step <andystep2008@gmail.com>
2023-01-10 20:20:54 +0200

    update jsx sb.jsx

--- src/SwiperBootstrap/SwiperBootstrap.jsx
+++ src/SwiperBootstrap/SwiperBootstrap.jsx
@@ -35,7 +35,7 @@ class SwiperB extends Component {
   
         <div className="actionGet">
 
-          {actArr.map(item => <BoxActionGet key={item} box={item.items.box} imgItem={item.items.imgItem} nameT={item.items.name} price={item.items.price} locationItem={item.items.locationItem} nameClass={item.items.nameClass} inputNumb={item.items.inputNumb} />)}
+          {actArr.map(item => actArr.items.map(item => <BoxActionGet key={item} box={item.items.box} imgItem={item.items.imgItem} nameT={item.items.name} price={item.items.price} locationItem={item.items.locationItem} nameClass={item.items.nameClass} inputNumb={item.items.inputNumb} />))}
 
         </div>
   
