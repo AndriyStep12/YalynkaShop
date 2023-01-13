0f92e00cdab29fb7cece01b53568139c59db56cc
Andriy_Step <andystep2008@gmail.com>
2023-01-13 17:31:00 +0200

    update

--- src/Base/Actions/Actions.jsx
+++ src/Base/Actions/Actions.jsx
@@ -9,7 +9,7 @@ let actions = [{
                 nameT: 'Ялинка акційна1',
                 price: 10,
                 locationItem: 'Львів',
-                imgItem: '',
+                imgItem: 'https://life.pravda.com.ua/images/doc/7/e/7e76486-yalynka-1.jpg',
                 box: 'box-priceAct01',
                 inputNumb: 'inputAct01',
                 nameClass: 'nameAct01'
@@ -18,7 +18,7 @@ let actions = [{
                 nameT: 'Ялинка акційна2',
                 price: 10,
                 locationItem: 'Львів',
-                imgItem: '',
+                imgItem: 'https://blog.yalynka.shop/wp-content/uploads/2019/12/vidi-i-vidminnosti-materialiv-shtuchnih-yalinok_5df52ebd2e73f-496x694.jpeg',
                 box: 'box-priceAct02',
                 inputNumb: 'inputAct02',
                 nameClass: 'nameAct02'
@@ -27,7 +27,7 @@ let actions = [{
                 nameT: 'Ялинка акційна3',
                 price: 10,
                 locationItem: 'Львів',
-                imgItem: '',
+                imgItem: 'https://www.dolynaplastic.com.ua/wp-content/uploads/2021/10/IMG_8908-scaled.jpg',
                 box: 'box-priceAct03',
                 inputNumb: 'inputAct03',
                 nameClass: 'nameAct03'
@@ -45,7 +45,7 @@ let actions = [{
                 nameT: 'Іграшка акційна1',
                 price: 10,
                 locationItem: 'Львів',
-                imgItem: '',
+                imgItem: 'https://images.prom.ua/3487309450_w640_h640_nabor-elochnyh-sharov.jpg',
                 box: 'box-priceAct11',
                 inputNumb: 'inputAct11',
                 nameClass: 'nameAct11'
@@ -54,7 +54,7 @@ let actions = [{
                 nameT: 'Іграшка акційна2',
                 price: 10,
                 locationItem: 'Львів',
-                imgItem: '',
+                imgItem: 'https://content2.rozetka.com.ua/goods/images/big/20227576.jpg',
                 box: 'box-priceAct12',
                 inputNumb: 'inputAct12',
                 nameClass: 'nameAct12'
@@ -63,7 +63,7 @@ let actions = [{
                 nameT: 'Іграшка акційна3',
                 price: 10,
                 locationItem: 'Львів',
-                imgItem: '',
+                imgItem: 'https://img.auchan.ua/rx/q_90,w_104,ofmt_webp/https://auchan.ua/media/catalog/category/shariki.jpg',
                 box: 'box-priceAct13',
                 inputNumb: 'inputAct13',
                 nameClass: 'nameAct13'
@@ -81,7 +81,7 @@ let actions = [{
                 nameT: 'Гірлянди акційні1',
                 price: 10,
                 locationItem: 'Львів',
-                imgItem: '',
+                imgItem: 'https://img.auchan.ua/rx/q_90,w_192,ofmt_webp/auchan.ua/media/catalog/product/cache/06e22f492b2a163833afee5498a82bfa/3/2/3245676486254.jpg',
                 box: 'box-priceAct21',
                 inputNumb: 'inputAct21',
                 nameClass: 'nameAct21'
@@ -90,7 +90,7 @@ let actions = [{
                 nameT: 'Гірлянди акційні2',
                 price: 10,
                 locationItem: 'Львів',
-                imgItem: '',
+                imgItem: 'https://content2.rozetka.com.ua/goods/images/big/244576391.jpg',
                 box: 'box-priceAct22',
                 inputNumb: 'inputAct22',
                 nameClass: 'nameAct22'
@@ -99,7 +99,7 @@ let actions = [{
                 nameT: 'Гірлянди акційні3',
                 price: 10,
                 locationItem: 'Львів',
-                imgItem: '',
+                imgItem: 'https://jysk.ua/noviy-rik-ta-rizdvo/girlyandi-kimnatni/6114701-heulandit-6117601-aurvandil.jpg',
                 box: 'box-priceAct23',
                 inputNumb: 'inputAct23',
                 nameClass: 'nameAct23'
