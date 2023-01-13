6b6297b7d8b0c55fe5ef55ca099fa655b6ad073f
Andriy_Step <andystep2008@gmail.com>
2023-01-13 14:49:37 +0200

    update

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -53,7 +53,13 @@ function Home (){
                     }}><span>🎄Клікніть тут🎄</span><i></i></button>
                     <button className="downClick" onClick={function(){
                         
-                        
+                        window.scrollTo(0, 1000);
+
+                        window.scrollTo({
+                            top: 100,
+                            left: 100,
+                            behavior: 'smooth'
+                        });
 
                     }}>
                         ➜
